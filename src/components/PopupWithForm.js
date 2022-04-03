2a804042619301ff62e0fd5817c0fe6e52b7c56a
serega4517 <serega4517@yandex.ru>
2022-04-03 20:08:29 +0300

    feat: added popup components and the work of opening popups

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function PopupWithForm(props) {
+  return (
+    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
+      <div className="popup__container">
+        <h3 className="popup__title">{props.title}</h3>
+        <form className="popup__form popup__form-profile" name={props.name} autoComplete="off" noValidate>
+          {props.children}
+          <button className="popup__save-button" type="submit">Сохранить</button>
+        </form>
+        <button className="popup__close-button" type="button" />
+      </div>
+    </div>
+  )
+}
+
+export default PopupWithForm;
