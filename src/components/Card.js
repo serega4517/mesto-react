8f26623b4dda4521c0d6631f9abfb556809dc584
serega4517 <serega4517@yandex.ru>
2022-04-04 14:20:01 +0300

    feat: added the opening popup with a full-size picture when clicking on the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,16 @@ import React from "react";
 
 function Card(props) {
 
+  function handleClick() {
+    props.onCardClick(props.card);
+  }
+
   return (
     <div className="element">
       <img className="element__image"
            src={props.card.link}
            alt={props.card.name}
+           onClick={handleClick}
       />
         <h2 className="element__title">{props.card.name}</h2>
         <div className="element__like-container">
